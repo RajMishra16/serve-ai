90b5eddf37d25321cbd2a03affae510edaa0060b
Shashank Das <shashankdas319@gmail.com>
2026-04-07 20:11:25 +0530

    fix api baseURL for production

--- frontend/services/user.service.ts
+++ frontend/services/user.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 export const getUserProfile = async () => {
   const response = await axios.get(`${API_BASE}/user`);
